820d7f5d7ec2a9784bc4957823fb30b887350819
MohammedHamim <99599371+BlackdreamIO@users.noreply.github.com>
2024-04-10 00:53:51 +0600

    modfied

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import AppView from './pages/app/AppView';
 import Webview from './pages/web/Webview';
 
 export default function page() 
 {
     return (
-        <div>
-            <Webview />
+        <div className='bg-black min-h-screen max-h-auto'>
+            {/* <Webview /> */}
+            <AppView/>
         </div>
     )
 }
