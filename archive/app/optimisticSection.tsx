0961397ab3b0f5520d927d9342c5378adf9ae96e
MohammedHamim <99599371+BlackdreamIO@users.noreply.github.com>
2024-05-18 19:06:15 +0600

    deleted

--- archive/app/optimisticSection.tsx
+++
@@ -1,6 +0,0 @@
-const [optimisticSections, updateOptimisticSections] = useOptimistic<SectionScheme[], SectionScheme>(
-    contextSections || [],
-    (currentState, updatedSection) => {
-        return [...currentState, updatedSection];
-    }
-);
